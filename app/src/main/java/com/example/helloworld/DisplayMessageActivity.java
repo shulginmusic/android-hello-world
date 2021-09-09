afcd7649038f742354ead4e60dad6336925cb78c
shulginmusic <efim@shulginmusic.com>
2021-09-08 23:04:26 -0700

    App finished!
    Message is successfully displayed when clicking button and the Up button navigates back to Main activity

--- app/src/main/java/com/example/helloworld/DisplayMessageActivity.java
+++ app/src/main/java/com/example/helloworld/DisplayMessageActivity.java
@@ -2,7 +2,9 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * This activity is accessed by the Intent object in MainActivity (see sendMessage() in that class)
@@ -13,5 +15,14 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        //Get the intent that led to (started) this activity and get the string (message) from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Get the text view and set the message
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+
     }
 }
