8396ee45f0bb098a01de7f06c6f7453009d914a7
shulginmusic <efim@shulginmusic.com>
2021-09-08 22:30:38 -0700

    First Commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when user presses button
+     * @param view
+     */
+    public void sendMessage(View view) {
+
+    }
+
+
+}
